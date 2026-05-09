d38c8390454061bd25ffe425fb7fb1ff75d5c303
expoders <expoders4@gmail.com>
2026-05-09 17:36:36 +0530

    changes layout and home

--- app/(user)/layout.tsx
+++ app/(user)/layout.tsx
@@ -1,15 +1,15 @@
 // src/app/(user)/layout.tsx
 
-import Footer from "../../components/layout/footer";
-import Header from "../../components/layout/header";
+// import Footer from "../../components/layout/footer";
+// import Header from "../../components/layout/header";
 export default function UserLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <Header />
+      {/* <Header /> */}
       <main className="min-h-screen pt-[var(--header-height)]">
         {children}
       </main>
-      <Footer />
+      {/* <Footer /> */}
     </>
   )
 }
